4f91dd0ebd7225807d73b32842d0b7276b69d194
cardiscardis <giftedcardis@outlook.com>
2024-01-24 08:03:11 +0100

    complete backend7

--- api/index.js
+++ api/index.js
@@ -5,7 +5,17 @@ const mongoose = require('mongoose');
 
 
 const app = express();
-app.use(cors())
+
+app.use(function (req, res, next) {
+  let origin = req.headers.origin;
+  res.header("Access-Control-Allow-Origin", req.headers.host.indexOf("localhost") > -1 ? "http://localhost:3000" : origin);
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+// app.use(cors())
 const PORT = process.env.PORT || 3001;
 
 const mongoURI = 'mongodb+srv://ibiekay2:sonorous@ultimategrants.przia9q.mongodb.net/test?retryWrites=true&w=majority';
